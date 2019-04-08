19fca3a5fa9cdc4260a80bc46ea661f897e3a911
TommyCakes <tomhwarhurst@gmail.com>
2019-04-08 21:39:41 +0100

    created basic layout

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -5,9 +5,9 @@ const MovieListItem = ({ movie }) => {
     const imgUrl = `https://image.tmdb.org/t/p/w342/${poster_path}`;
 
     return (
-        <li key={movie.id} className="movie-item">
+        <li key={id} className="movie-item">
             <img src={imgUrl} alt=""/>
-            <span>{movie.title}</span>
+            <span>{title}</span>
         </li> 
     )
 }
