982b641b4db28b517ad69bbb41bb971669a6c974
TommyCakes <tomhwarhurst@gmail.com>
2019-04-09 12:45:06 +0100

    moved state to main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,10 +7,52 @@ const mainStyle = {
 }
 
 export default class Main extends Component {
+    state = {
+        genre: "horror",
+        genres: [],
+        year: {
+            label: "year",
+            min: 1940,
+            max: 2019,
+            step: 1,
+            value: { min: 2000, max: 2019 }
+        },
+        rating: {
+            label: "rating",
+            min: 0,
+            max: 10,
+            step: 1,
+            value: { min: 7, max: 10 }            
+        },
+        runtime: {
+            label: "runtime",
+            min:0,
+            max: 300,
+            step: 15,
+            value: { min: 120, max: 240 }
+        }
+    }
+
+    onGenreChange = event => {
+        this.setState({ genre: event.target.value });
+    }
+
+    onChange = data => {
+        this.setState({
+            [data.type]: {
+                ...this.state[data.type],
+                value: data.value
+            }
+        });
+    };
+
     render() {
         return (
             <section style={mainStyle}>
-                <Navigation />
+                <Navigation 
+                    onChange={this.onChange}
+                    onGenreChange={this.onGenreChange}
+                    {...this.state}/>
                 <Movies />
             </section>
         )
